2cd62d112ebcce4c275f947f6ce851db00722b77
Bhesh Raj <rajbhesh566@gmail.com>
2023-07-20 01:30:52 +0545

    appinment starting

--- controllers/healthController.js
+++ controllers/healthController.js
@@ -1,1 +1,121 @@
-//inital
+//const multer = require('multer');
+
+const Appoinment = require('../Models/appointmentModel');//
+const User = require('../Models/userModel');//
+
+const catchAsync = require('../utils/catchAsync')
+const AppError = require('./../utils/appError')
+
+
+exports.bookAppointment= catchAsync(async(req,res,next)=>{
+      
+    const user = await User.findOne({email:req.body.email})
+        if(!user){
+            return next(new AppError('please signup with this mai'),400);
+        }
+    const patientDetails = await Appoinment.create({
+        name:user.name,
+        email:user.email,
+        phone:user.phone,
+        address:user.address,
+        age:user.age,
+        gender:user.gender,
+        serviceTitle:req.body.serviceTitle,
+        appointmentDate:req.body.appointmentDate,
+        message:req.body.message,
+        doctor_code:req.body.doctor_code
+
+    });
+
+         res.status(200).json({
+             status:"success",
+            // number:patientDetails.length,
+             data:{
+                 //patientDetails
+                 user
+             }
+         })
+     
+        })
+
+
+
+
+exports.getAppointments= async(req, res)=>{
+    
+    const allAppointments = await Appoinment.find()
+    
+        if(!allAppointments){
+             res.json({messgae:"No appointments found"})
+        } 
+        else{
+         res.render("../views/Routine/routine.ejs",{
+             
+             AppoinmentData:allAppointments,
+              title:"Appoinments"
+         })
+     } 
+     }
+
+
+
+     exports.updateAppointment= async(req, res)=>{
+    
+
+        const appoinment =  await Appoinment.findById(req.params.id)
+  
+  
+           if( appoinment== null){
+                res.redirect('http://127.0.0.1:3007/api/v1/routine');
+           } 
+           else{
+            res.render("../views/Routine/edit_period.ejs",{
+                 
+              period:period,
+              title:"Edit Period"
+            })
+        } 
+  
+      
+      
+       }
+  
+  
+  exports.updatePeriod= (async(req, res)=>{    
+      
+   //console.log(req.files);
+      //if image is not changed,old img jo save garna paryo
+      if(req.files){
+          
+          req.body.image= req.files.image.name;
+      }
+   
+      try{
+          const updatedPeriod = await Routine.findByIdAndUpdate(req.params.id , req.body,{
+                  new:true,
+                  runValidator:true
+              
+          })
+         
+  
+          if(updatedPeriod)
+          {
+                  req.session.message={
+                      type:'success',
+                      message:'Period update successfully'
+              }
+              } 
+  }
+  catch(err){
+      
+        
+              res.json({message:err.message , type:'danger'})
+          
+          }
+  
+    res.redirect('http://127.0.0.1:3007/api/v1/routine')
+     
+      })  
+  
+      
+ 
---
+++ utils/email.js
@@ -0,0 +1,112 @@
+
+
+
+
+// Building a Complex Email Handler
+const nodemailer = require('nodemailer') 
+const ejs = require('ejs');
+const expressSanitizer = require('express-sanitizer');
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_PASSWORD);
+
+module.exports = class Email{
+  constructor(user , url){//userDetalis , resteUrl
+     this.to = user.email;
+     this.firstName = user.name.split(' ')[0];
+     this.url=url;
+     this.from = `Bhesh Raj Neupane <${process.env.EMAIL_FROM}>`;
+    // console.log(htmlToText)
+  }
+
+  newTransport(){
+      // if(process.env.NODE_ENV === 'production'){
+      //    //Sendgrid
+      //     return nodemailer.createTransport({
+      //       service:'SendGrid',
+      //       auth:{
+      //         user:process.env.SENDGRID_USERNAME,
+      //         pass:process.env.SENDGRID_PASSWORD
+      //      }
+      //    })
+      // }
+
+      //development mode ;test on mailptrap using nodemailer
+    return nodemailer.createTransport({
+         // service:'Gmail',
+         host:process.env.EMAIL_HOST,
+         port:process.env.EMAIL_PORT,
+         auth:{
+            user:process.env.EMAIL_USERNAME,
+            pass:process.env.EMAIL_PASSWORD   
+          }
+         });
+       
+     }
+
+
+  //Send the actual email 
+   send(template , subject) {
+  
+      //1) Render HTML based templates
+          console.log(template)
+       
+       ejs.renderFile(__dirname+`../views/email/${template}.ejs `, 
+         {
+         firstName :this.firstName,
+         url : this.url,
+         subject
+         } ,  async (err,data)=>{
+            console.log(data);
+            const mailOptions = { 
+               from: this.from,
+               to : this.to ,
+               subject:subject,
+               html:data,
+               //text: 
+               
+               }
+               await this.newTransport().sendMail(mailOptions)
+         })
+
+      //2) Define email options
+      // const mailOptions = { 
+      //    from: this.from,
+      //    to : this.to ,
+      //    subject:subject,
+      //    html:html,
+      //    //text: 
+         
+      //    }
+
+      //3) Create a transport and send email
+      // await this.newTransport().sendMail(mailOptions)
+     //Note:sendMail was built in fxn to send email , which takes one arg as option for mail
+      } //sendClosed
+
+
+      async sendWelcome(){
+         //send(template , subject)
+         //welcome-->tempalte which created
+         await  this.send('welcome' , 'WELCOME TO SMART CITY ..THANK YOU FOR JOINING WITH US!')
+       }
+      async sendaAppoinmentMail(){
+         //send(template , subject)
+         //welcome-->tempalte which created
+         await  this.send('appoinment' , 'Your appoinment is booked!')
+       }
+      async sendPasswordReset(){
+      await this.send('passwordReset','Your password reset token(valid for only 10 minutes)')
+   }
+
+}// class Closed
+
+
+
+
+
+
+
+
+
+
+
